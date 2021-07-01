6d310f533f076e86cbccaffce89099526857bd1b
Antipow <antipow@gmail.com>
2021-07-01 16:52:22 +0300

    9.3

---
+++ src/CommentForm/CommentForm.tsx
@@ -0,0 +1,22 @@
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+import styles from './commentform.css';
+
+export function CommentForm() {
+    const [value, setValue] = useState('');
+
+    function handleChange(event: ChangeEvent<HTMLTextAreaElement>){
+        setValue(event.target.value);
+    }
+
+    function handleSubmit(event: FormEvent){
+        event.preventDefault();
+        console.log(value);
+    }
+
+    return (
+        <form className={styles.form} onSubmit={handleSubmit}>
+            <textarea className={styles.input} value={value} onChange={handleChange}/>
+            <button type="submit" className={styles.button}>Комментировать</button>
+        </form>
+    );
+}
